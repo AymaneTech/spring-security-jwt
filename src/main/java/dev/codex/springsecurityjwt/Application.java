3b48c1da54ba89cad5a4d700efec51dc64ab8c2c
geek <elmainiaymane03@gmail.com>
2024-07-14 13:42:25 +0100

    setup the application yml

--- src/main/java/dev/codex/springsecurityjwt/Application.java
+++ src/main/java/dev/codex/springsecurityjwt/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityJwtApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
