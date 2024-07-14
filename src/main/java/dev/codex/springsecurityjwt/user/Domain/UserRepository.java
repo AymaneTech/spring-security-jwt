b932e9f83b0c75e0679c59892df8db46aa11b4cc
geek <elmainiaymane03@gmail.com>
2024-07-14 16:16:26 +0100

    make the user repository public to use it in user details implementation

--- src/main/java/dev/codex/springsecurityjwt/user/Domain/UserRepository.java
+++ src/main/java/dev/codex/springsecurityjwt/user/Domain/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
