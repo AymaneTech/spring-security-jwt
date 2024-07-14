b1eb2caa13c54b06a9d2132d1ffab7872222edbf
geek <elmainiaymane03@gmail.com>
2024-07-14 16:29:42 +0100

    choose the defualt authentication manager of spring security

--- src/main/java/dev/codex/springsecurityjwt/config/ApplicationConfig.java
+++ src/main/java/dev/codex/springsecurityjwt/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import dev.codex.springsecurityjwt.user.Domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ApplicationConfig {
 
     private final UserRepository userRepository;
+    private final AuthenticationConfiguration config;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -29,6 +32,11 @@ public class ApplicationConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
